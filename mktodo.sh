5f775983930d451ce0843533907bf73c86135473
Gareth Nelson <gareh@garethnelson.com>
2015-05-24 06:39:57 +0100

    don't include TODO in mktodo.sh output

--- mktodo.sh
+++ mktodo.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-ls usr.bin/ obj/bsdtools/_install/usr/bin/ | xargs -n 1 | sort  | uniq -u | grep -v : >usr.bin/TODO
-ls bin/ obj/bsdtools/_install/bin/ | xargs -n 1 | sort  | uniq -u | grep -v : >bin/TODO 
+ls usr.bin/ obj/bsdtools/_install/usr/bin/ | xargs -n 1 | sort  | uniq -u | grep -v : | grep -v TODO >usr.bin/TODO
+ls bin/ obj/bsdtools/_install/bin/ | xargs -n 1 | sort  | uniq -u | grep -v : | grep -v TODO >bin/TODO 
